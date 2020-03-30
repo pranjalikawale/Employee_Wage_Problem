51cb52ab32ff8b9253467b9351c9cd4bfdac75b9
pranjalikawale <pranjalikawale1578@gmail.com>
2020-03-29 14:37:49 -1200

    ADDED: Calculate the monthly wages of employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,21 +3,28 @@
 FULL_TIME=1
 PART_TIME=2
 WAGE_PER_HOUR=20
+WORKING_DAY=20
+salary=0
 
-checkAttendence=$((RANDOM%3))
+for (( i=1;i<=WORKING_DAY;i++ ))
+do
+	checkAttendence=$((RANDOM%3))
 
-case $checkAttendence in
-	$FULL_TIME)
-		TOTAL_HOUR=8
-		;;
-	$PART_TIME)
-		TOTAL_HOUR=4
-		;;
-	*)
-		TOTAL_HOUR=O
-		;;
-esac
+	case $checkAttendence in
+		$FULL_TIME)
+			TOTAL_HOUR=8
+			;;
+		$PART_TIME)
+			TOTAL_HOUR=4
+			;;
+		*)
+			TOTAL_HOUR=O
+			;;
+	esac
 
-salary=$(($WAGE_PER_HOUR*$TOTAL_HOUR))
-echo "Daily wage of employee is "$salary
+	payPerDay=$(($WAGE_PER_HOUR*$TOTAL_HOUR))
+	salary=$(($salary+$payPerDay))
+done
+
+echo "Monthly wage of employee is "$salary
 
