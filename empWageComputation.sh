57b25885db852849fa4e74a940b235450f9a3491
pranjalikawale <pranjalikawale1578@gmail.com>
2020-03-29 13:20:30 -1200

    ADDED: Calculating daily employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,11 +3,14 @@
 ISPRESENT=1
 checkAttendence=$((RANDOM%2))
 
-if [ $ISPRESENT -eq $checkAttendence ]
+if [	$ISPRESENT -eq $checkAttendence	]
 then
-	echo "Employee is Present"
+	WAGE_PER_HOUR=20
+	TOTAL_HOUR=8
+	salary=$(($WAGE_PER_HOUR*$TOTAL_HOUR))
 else
-	echo "Employee is Absent"
+	salary=O
 fi
 
+echo "Daily wage of employee is "$salary
 
